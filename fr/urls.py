a5b38118575377603393835abadf131d5cd6435b
Morgan Robin <th4rkunn@gmail.com>
2015-08-10 17:30:43 +0200

    Correction mineure

--- fr/urls.py
+++ fr/urls.py
@@ -11,9 +11,9 @@ urlpatterns = [
 	url(r'^erreur$', views.Erreur, name='erreur'), # Vue appelée lorsque qu'une erreur est soulevée
 	url(r'^login$', views.Login,  name='login'), # Vue appelée pour se logger
 	url(r'^index_secure$', views.Index_secure, name='index_secure'), # Vue de l'index APRES login
-	url(r'^ajout/1$', views.Ajout, name='ajout_1'), # Vue pour ajouter une machine au LDAP, étape 1
-	url(r'^ajout/2$', views.Ajout, name='ajout_2'), # Vue pour ajouter une machine au LDAP, étape 2
-	url(r'^ajout/3$', views.Ajout, name='ajout_3'), # Vue pour ajouter une machine au LDAP, étape 3
+	url(r'^ajout/1$', views.Ajout_1, name='ajout_1'), # Vue pour ajouter une machine au LDAP, étape 1
+	url(r'^ajout/2$', views.Ajout_2, name='ajout_2'), # Vue pour ajouter une machine au LDAP, étape 2
+	url(r'^ajout/3$', views.Ajout_3, name='ajout_3'), # Vue pour ajouter une machine au LDAP, étape 3
 	url(r'^reactivation$', views.Reactivation, name='reactivation'), # Vue pour réactiver une machine
 	url(r'^devenir_membre$', views.Devenir_membre, name='devenir_membre'), # Vue pour devenir membre du ResEl
 ]
