b9e53910748e3305bb039d14e6a7535e01dce838
Morgan Robin <th4rkunn@gmail.com>
2015-08-20 12:33:01 +0200

    Modif de la récupération d'adresse mac

--- fr/network.py
+++ fr/network.py
@@ -23,6 +23,7 @@ def get_mac_from_ip(request, ip, local_net):
             return None
 
         mac = Popen(["arping -c 1 -I eth2".format(ip)], stdout=PIPE, shell=True).communicate()[0].split('from')[1].split(' ')[0].lower()
+        print mac
 
         if not mac:
             messages.error(request, "Aucun resultat avec l'hote {}".format(ip))
