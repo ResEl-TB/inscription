908099f801ae28974cf6648e7b1366d28458ab3a
Morgan Robin <th4rkunn@gmail.com>
2015-08-21 00:35:21 +0200

    Supression de l'accès à l'interface admin, car useless pour ce site

--- inscription/urls.py
+++ inscription/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'fr.views.Verification'),
     url(r'^fr/', include('fr.urls', namespace='fr')),
     url(r'^en/', include('en.urls', namespace='en')),
