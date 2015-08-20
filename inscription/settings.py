106fd287a36f38c09fecec78130428182c2bcb24
Morgan Robin <th4rkunn@gmail.com>
2015-08-21 00:55:30 +0200

    Static directory

--- inscription/settings.py
+++ inscription/settings.py
@@ -134,8 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "sfiles"),
 )
 
 # Expiration de session pour les users connectés
