fc1f58a5853ff93ebf6bf0e1b6939d90a5ca1c99
Morgan Robin <th4rkunn@gmail.com>
2015-08-21 23:33:25 +0200

    Test pour utiliser le CAS de l'école

--- fr/urls.py
+++ fr/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.Verification, name='verification'), # Vue d'accueil, qui teste si on a bien une IP ResEl...
+	url(r'^$', 'django_cas_ng.views.login', name='verification'), # Vue d'accueil, qui teste si on a bien une IP ResEl...
 	url(r'^index$', views.Index, name='index'), # Vue appelée lorsque tout est en règle
 	url(r'^erreur$', views.Erreur, name='erreur'), # Vue appelée lorsque qu'une erreur est soulevée
 	url(r'^contact$', views.Contact, name="contact"), # Vue pour afficher un formulaire d'envoi de mail pour nous contacter
-	url(r'^login$', 'django_cas_ng.views.login',  name='login'), # Vue appelée pour se logger
+	url(r'^login$', views.Login,  name='login'), # Vue appelée pour se logger
 	url(r'^index_secure$', views.Index_secure, name='index_secure'), # Vue de l'index APRES login
 	url(r'^ajout/1$', views.Ajout_1, name='ajout_1'), # Vue pour ajouter une machine au LDAP, étape 1
 	url(r'^ajout/2$', views.Ajout_2, name='ajout_2'), # Vue pour ajouter une machine au LDAP, étape 2
