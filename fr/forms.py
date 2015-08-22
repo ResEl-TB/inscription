bb8eceb5c0f5278c224558e2e6769a55c3806409
Morgan Robin <th4rkunn@gmail.com>
2015-08-22 18:23:44 +0200

    Corrections

--- fr/forms.py
+++ fr/forms.py
@@ -43,30 +43,32 @@ class AliasForm(forms.Form):
     def clean_alias1(self):
         alias = self.cleaned_data['alias1'].lower()
         
-        if re.search(r'^[a-z][a-z0-9-]{0,23}[a-z0-9]', alias) is None:
-            raise forms.ValidationError("L'alias {} ne correspond pas à la forme attendue.".format(alias))
-        else:
-            if re.search(r'enst-bretagne', alias):
-                raise forms.ValidationError("L'alias ne doit pas contenir le nom de l'école.")
-            if re.search(r'resel', alias):
-                raise forms.ValidationError("L'alias ne doit pas contenir le nom resel.")
-            if search("ou=machines,dc=resel,dc=enst-bretagne,dc=fr", "(hostAlias={})".format(alias)) is not None:
-                raise forms.ValidationError("L'alias choisi est déjà utilisé pour une machine de notre réseau.")
+        if alias != '':
+            if re.search(r'^[a-z][a-z0-9-]{0,23}[a-z0-9]', alias) is None:
+                raise forms.ValidationError("L'alias {} ne correspond pas à la forme attendue.".format(alias))
+            else:
+                if re.search(r'enst-bretagne', alias):
+                    raise forms.ValidationError("L'alias ne doit pas contenir le nom de l'école.")
+                if re.search(r'resel', alias):
+                    raise forms.ValidationError("L'alias ne doit pas contenir le nom resel.")
+                if search("ou=machines,dc=resel,dc=enst-bretagne,dc=fr", "(hostAlias={})".format(alias)) is not None:
+                    raise forms.ValidationError("L'alias choisi est déjà utilisé pour une machine de notre réseau.")
         
         return alias
 
     def clean_alias2(self):
         alias = self.cleaned_data['alias2'].lower()
-        
-        if re.search(r'^[a-z][a-z0-9-]{0,23}[a-z0-9]', alias) is None:
-            raise forms.ValidationError("L'alias {} ne correspond pas à la forme attendue.".format(alias))
-        else:
-            if re.search(r'enst-bretagne', alias):
-                raise forms.ValidationError("L'alias ne doit pas contenir le nom de l'école.")
-            if re.search(r'resel', alias):
-                raise forms.ValidationError("L'alias ne doit pas contenir le nom resel.")
-            if search("ou=machines,dc=resel,dc=enst-bretagne,dc=fr", "(hostAlias={})".format(alias)) is not None:
-                raise forms.ValidationError("L'alias choisi est déjà utilisé pour une machine de notre réseau.")
+
+        if alias != '':
+            if re.search(r'^[a-z][a-z0-9-]{0,23}[a-z0-9]', alias) is None:
+                raise forms.ValidationError("L'alias {} ne correspond pas à la forme attendue.".format(alias))
+            else:
+                if re.search(r'enst-bretagne', alias):
+                    raise forms.ValidationError("L'alias ne doit pas contenir le nom de l'école.")
+                if re.search(r'resel', alias):
+                    raise forms.ValidationError("L'alias ne doit pas contenir le nom resel.")
+                if search("ou=machines,dc=resel,dc=enst-bretagne,dc=fr", "(hostAlias={})".format(alias)) is not None:
+                    raise forms.ValidationError("L'alias choisi est déjà utilisé pour une machine de notre réseau.")
 
         return alias
 
