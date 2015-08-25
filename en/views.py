8741e93bc992646324ec05f11bcae1df280652df
Morgan Robin <th4rkunn@gmail.com>
2015-08-25 17:32:02 +0200

    Correction get_ip pour envoi de mail lors de la réactivation

--- en/views.py
+++ en/views.py
@@ -250,8 +250,8 @@ def Reactivation(request):
         mod(machine[0], mod_attrs)
         update_dhcp_dns_firewall()
 
-        hostname = machine[1]['host']
-        ip = machine[1]['iphostnumber']
+        hostname = machine[0][1]['host']
+        ip = machine[0][1]['ipHostNumber']
 
         mail = EmailMessage(
                 subject="[Inscription Brest] Reactivation machine {} [172.22.{} - {}] par {}".format(hostname, ip, request.session['mac_client'], request.session['uid_client']),
